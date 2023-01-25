858cde9a65a531d40efbe717e44d4365f051c308
mayankSpeckyfox <mayank.singh@speckyfox.com>
2023-01-25 13:38:07 +0530

    final

--- server/db.js
+++ server/db.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
 const connect = () => {
-    mongoose.connect(process.env.DB).then(() => {
-        console.log("Database connected successfully");
-    }).catch((err) => {
-        throw err;
+  mongoose
+    .connect(process.env.DB)
+    .then(() => {
+      console.log("Database connected successfully");
     })
-}
+    .catch((err) => {
+      throw err;
+    });
+};
 export default connect;
