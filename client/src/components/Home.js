9b351ef6af4a464c965be6e53bcfa029d29acabf
mayankSpeckyfox <mayank.singh@speckyfox.com>
2023-02-07 18:20:16 +0530

    added home animation

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,37 @@
 import React from "react";
 import "../css/Home.css";
+import Footer from "./footer/Footer.js";
+import { Stack } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="home-content">
         <div className="container-fluid">
-          <h1>Welcome</h1>
+          <div className="p-4 page-nav">
+            <Stack direction="row" sx={{ justifyContent: "space-between" }}>
+              <Stack direction="row" spacing={2}>
+                <KeyboardBackspaceIcon
+                  onClick={() => navigate(-1)}
+                  sx={{
+                    fontSize: "x-large",
+                    color: "brown",
+                    cursor: "pointer",
+                  }}
+                />
+
+                <span className="text-muted ">
+                  <b>
+                    <span className="asset-head"> ASSET </span>{" "}
+                    <span className="asset-head-b">MANAGEMENT </span>{" "}
+                  </b>
+                </span>
+              </Stack>
+            </Stack>
+          </div>
+          <Footer />
         </div>
       </div>
     </>
