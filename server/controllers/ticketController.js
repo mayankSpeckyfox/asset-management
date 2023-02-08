688765da698062b67ea62b9f68736a8129a2c11c
mayankSpeckyfox <mayank.singh@speckyfox.com>
2023-02-08 16:47:23 +0530

    added send email api

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -1,5 +1,37 @@
 import Ticket from "../models/Ticket.js";
+import nodemailer from "nodemailer";
+//send email
+export const sendEmail = async (req, res) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+    const ms = Date.now();
+    const date = Date(ms);
 
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: req.body.email,
+      subject: "Ticket",
+      text: `A NEW ticket has been generated on ${date}! `,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        res.status(500).json({ message: "There was an error sending email" });
+      } else {
+        res.status(200).json({ message: "Email sent successfully" });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "sorry error occured" });
+  }
+};
 // create ticket
 export const createTicket = async (req, res) => {
   try {
--- server/routes/Ticketroutes.js
+++ server/routes/Ticketroutes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { createTicket } from "../controllers/ticketController.js";
+import { createTicket, sendEmail } from "../controllers/ticketController.js";
 const router = express.Router();
+//send email
+router.post("/sendemail", sendEmail);
 // create ticket
 router.post("/create", createTicket);
 export default router;
